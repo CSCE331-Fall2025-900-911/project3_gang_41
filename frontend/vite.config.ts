04788bc5cdd2a8b2ae2b87fea81df8c5e7ab2698
jasonlau05 <jasonlaucr@gmail.com>
2025-11-07 12:44:24 -0600

    initialized shadcn

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173
-  }
+  },
+  resolve: {
+    alias: {
+      // Maps the '@' alias to the absolute path of the 'src' directory
+      "@": path.resolve(__dirname, "./src"), 
+    },
+  },
 });
