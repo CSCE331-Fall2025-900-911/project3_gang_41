2658ffb6c211fa946180fd035fea6268ed1282e3
jasonlau05 <jasonlaucr@gmail.com>
2025-11-10 22:34:58 -0600

    kiosk

--- frontend/src/components/sidebar.tsx
+++ frontend/src/components/sidebar.tsx
@@ -1,9 +1,85 @@
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar"
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
 
-export function Menusidebar() {
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarFooter,
+} from "@/components/ui/sidebar"
+import { useNavigate } from "react-router-dom"
+
+// Menu items.
+const items = [
+  {
+    title: "Home",
+    url: "",
+    icon: Home,
+  },
+  {
+    title: "All Items",
+    url: "all",
+    icon: Inbox,
+  },
+  {
+    title: "Milk Tea",
+    url: "milktea",
+    icon: Calendar,
+  },
+  {
+    title: "Fruit Tea",
+    url: "fruity",
+    icon: Search,
+  },
+  {
+    title: "Coffee",
+    url: "coffee",
+    icon: Settings,
+  },
+  {
+    title: "Smoothies",
+    url: "smoothies",
+    icon: Settings,
+  },
+  {
+    title: "Seasonal",
+    url: "seasonal",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  const navigate = useNavigate()
   return (
     <Sidebar>
-      <SidebarContent />
+      <SidebarHeader>
+        this is my sidebar lil bro
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
     </Sidebar>
+    
   )
 }
--- frontend/src/kioskmenu.tsx
+++ frontend/src/kioskmenu.tsx
@@ -1,22 +1,30 @@
 import { useNavigate } from "react-router-dom";
-import {
-  Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
-  SidebarHeader,
-} from "@/components/ui/sidebar"
+import { Button } from "@/components/ui/button"
+import { SidebarProvider} from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/sidebar"
 
 export default function kioskmenu() {
     const navigate = useNavigate()
+
     return (
-        <Sidebar>
-        <SidebarHeader />
-        <SidebarContent>
-            <SidebarGroup />
-            <SidebarGroup />
-        </SidebarContent>
-        <SidebarFooter />
-        </Sidebar>
+        <div className="flex min-h-screen">
+            <AppSidebar />
+        
+            <main className="flex-1 flex items-center justify-center w-full relative">
+                <div>
+                    hello world
+                </div>
+
+
+                <div className="fixed bottom-0 left-0 right-0 h-[10%] bg-gray-500 flex items-center justify-center">
+                    <div className="flex items-center justify-center gap-4 w-full max-w-[500px] px-4">
+                        <span className="text-white">Total:</span>
+                        <Button variant="default" className="flex-1 max-w-[200px]">
+                            Checkout
+                        </Button>
+                    </div>
+                </div>
+            </main>
+        </div>
     )
 }
