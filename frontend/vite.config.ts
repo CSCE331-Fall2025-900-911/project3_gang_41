c853d52e3f5bbf82b723482741e690fd58669c3c
WilliamT-byte <williamt2023@tamu.edu>
2025-11-12 23:53:34 -0600

    updated vite config to deal with COOP

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,10 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173
+    port: 5173,
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin-allow-popups',
+    },
   },
   resolve: {
     alias: {
